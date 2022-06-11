5dee1f9d1e5f1b51b2a55203a6f70802d162c126
verah <verahnyagaka101@gmail.com>
2022-06-12 01:50:23 +0300

    Assignment completed

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,40 +1,124 @@
 function gameObject(){
     return {
+        //home
         home: {
-          teamName: "Brooklyn Nets",
-          colors: ["black","white"],
-          players:  {
-             "Verah": {
-                  Number: " 3",
-                  shoe: " bata",
-                  points: " 7",
-                 rebounds: " 1",
-                 assists: " 2",
-                 steals: " 1",
-                 blocks: " 0",
-                 slamdunks: " 4",
-              }
-          }
+            teamName: "Brooklyn Nets",
+            colors: ['black', 'white'],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounce: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slumDunks: 1
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounce: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slumDunks: 7
+                },
+                "Brook Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounce: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slumDunks: 15
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounce: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slumDunks: 5
+                },
+                "Jason Terry": {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounce: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slumDunks: 1
+                }
+            }
         },
+
+        //away
         away: {
             teamName: "Charlotte Hornets",
-            colors: ["Turquoise","Purple"],
-            players:  {
-                name: {
-                    Name: "kwamboka",
-                    Number: "9",
-                    shoe: "bata",
-                    points: "39",
-                    rebounds: "2",
-                    assists: "14",
-                    steals: "1",
-                    blocks: "2",
-                    slamdunks: "1",
+            colors: [ "turquoise", "purple"],
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounce: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slumDunks: 2
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounce: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slumDunks: 10
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounce: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slumDunks: 5
+                },
+                "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounce: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slumDunks: 0
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounce: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slumDunks: 12
                 }
             }
         }
+    
     }
 }
+
 
 function homeTeamName(){
     return gameObject()['home']['teamName']
@@ -43,11 +127,11 @@ function homeTeamName(){
 
 function numPointsScored(playerName){
     let players = {...gameObject()['home']['players'], ...gameObject()['away']['players']}
-    return `${playerName} scored ${players[playerName].points} points`
+    return `${players[playerName].points} `
 }
 function shoeSize(playerName){
     let players = {...gameObject()['home']['players'], ...gameObject()['away']['players']}
-    return `${playerName} shoe size is ${players[playerName].shoe}`
+    return `${players[playerName].shoe}`
 }
 function teamColors(teamName){
     let game = gameObject()
@@ -58,3 +142,23 @@ function teamColors(teamName){
         }
     }
 }
+
+// function teamNames(){
+
+// }
+
+// function playerNumbers(){
+
+// }
+// function playerStats(){
+
+// }
+// function bigShoeRebounds(){
+
+// }
+// function mostPointsScored(){
+
+// }
+// winningTeam
+// playerWithLongestName
+// doesLongNameStealATon
